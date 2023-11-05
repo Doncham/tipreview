7ded19a5ea2c1bec781968b0b02ca026c933140a
Sangwon <qmfdnjssk@tukorea.ac.kr>
2023-11-05 19:59:31 +0900

    feat : 임시 저장

--- src/test/java/tipview/toyproject/repository/MemberRepositoryTest.java
+++ src/test/java/tipview/toyproject/repository/MemberRepositoryTest.java
@@ -4,12 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import tipview.toyproject.domain.Member;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+@TestPropertySource(properties = "src")
 @Transactional
 @SpringBootTest
 class MemberRepositoryTest {
